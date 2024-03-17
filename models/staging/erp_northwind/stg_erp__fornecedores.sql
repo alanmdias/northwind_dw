bbaaafd13881a24d5384a35677c787e66d3e8baf
Alan Dias <alandias.m@gmail.com>
2024-03-17 18:07:08 +0000

    mudar id_fornecedor para int

--- models/staging/erp_northwind/stg_erp__fornecedores.sql
+++ models/staging/erp_northwind/stg_erp__fornecedores.sql
@@ -1,7 +1,7 @@
 with
     fonte_fornecedores as (
         select
-            cast(supplier_id as string) as id_fornecedor,
+            cast(supplier_id as int) as id_fornecedor,
             cast(company_name as string) as nome_fornecedor,
             cast(contact_name as string) as contato_fornecedor,
             cast(contact_title as string) as contato_funcao,
